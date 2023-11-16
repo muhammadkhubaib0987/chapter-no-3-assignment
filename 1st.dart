cc98bbd7d35bbc641e4762323e6a239ce553a3f0
muhammadkhubaib0987 <mkhubaib889@gmail.com>
2023-11-17 02:54:15 +0500

    Create 1st.dart

---
+++ 1st.dart
@@ -0,0 +1,17 @@
+//Exercise # 1
+  //Question 1
+ 
+ //Write a program that calculates the area and perimeter of a rectangle given its length and width. Use the appropriate arithmetic operators.
+ 
+void main(List<String> args) {
+  // calculates the area of a rectangle??
+
+  int length = 40;
+  int width = 40;
+  int area = length * width;
+  print("the area of a rectangle is $area");
+  // calculates the perimeter of a rectangle??
+
+  int parimeter = 2 * (length + width);
+  print("the area of a rectangle is $parimeter");
+}
